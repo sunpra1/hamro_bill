fbda6f314be0f6a93959975fee6ea72dee75514d
sunpra1 <sunpra12@gmail.com>
2021-11-30 22:28:16 +0545

    Updates in print url

--- app/src/main/java/com/hamrobill/data/api/PrintApiConsumer.kt
+++ app/src/main/java/com/hamrobill/data/api/PrintApiConsumer.kt
@@ -7,6 +7,6 @@ import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface PrintApiConsumer {
-    @POST("print")
+    @POST
     suspend fun saveTableOrders(@Body body: SaveOrderRequest): Response<String>
 }
